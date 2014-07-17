7f5c707d68107a6362a141ca200fd519b02d9532
zhong717 <changzhong717@163.com>
2014-07-17 11:58:03 +0800

    注释掉一个函数

--- wx/wxgl.aspx.cs
+++ wx/wxgl.aspx.cs
@@ -237,7 +237,7 @@ namespace wx
             comm.ExecuteNonQuery();
             conn.Close();
             GridView3.EditIndex = -1;
-            bind_zlzk(); 
+          //  bind_zlzk(); 
         }
 
 
